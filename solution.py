739d8077776d5d99bec9c19a04e2ef13f8efb7c0
angelino-alpha <59427419+angelino-alpha@users.noreply.github.com>
2023-10-18 07:22:55 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,24 +4,23 @@ import pandas as pd
 database_file = '{path of the database.db}'
 
 try:
-	connection = sqlite3.connect(database_file)
-
-	sql_query = "SELECT c.customer_id as Customer, c.age as Age, i.item_id as Item, SUM(o.quantity) AS Quantity FROM customers c JOIN sales s ON c.customer_id = s.customer_id JOIN orders o ON s.sales_id = o.sales_id JOIN items i ON o.item_id = i.item_id GROUP BY c.customer_id, i.item_id ORDER BY c.customer_id, i.item_id;"
+    connection = sqlite3.connect(database_file)
+    sql_query = "SELECT c.customer_id as Customer, c.age as Age, i.item_id as Item, SUM(o.quantity) AS Quantity FROM customers c JOIN sales s ON c.customer_id = s.customer_id JOIN orders o ON s.sales_id = o.sales_id JOIN items i ON o.item_id = i.item_id GROUP BY c.customer_id, i.item_id ORDER BY c.customer_id, i.item_id;"
 		
-	df = pd.read_sql_query(sql_query, connection)
+    df = pd.read_sql_query(sql_query, connection)
 
-	df = df[df['Age'] >= 18 ]
-	df = df[df['Age'] <= 35 ]
-	df = df[df['Quantity'] > 0 ]
-	df = df[df['Quantity'].round() == df['Quantity']]
+    df = df[df['Age'] >= 18 ]
+    df = df[df['Age'] <= 35 ]
+    df = df[df['Quantity'] > 0 ]
+    df = df[df['Quantity'].round() == df['Quantity']]
 	
-	if not df.empty:
+    if not df.empty:
         print("Final Output:")
         print(df)
     else:
         print("No rows match the filter criteria.")
 
-	connection.close()
+    connection.close()
 
 except sqlite3.Error as e:
     print(f"SQLite error: {e}")
